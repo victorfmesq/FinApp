f84b88369516c284bc7c8bc7c562f82869a1db5b
victorfmesq <victor.mesquita@agilean.com.br>
2024-11-10 20:34:40 -0300

    Style: create app theme

---
+++ src/themes/index.ts
@@ -0,0 +1,24 @@
+// theme.js
+export const theme = {
+  light: {
+    primary: '#10B981', // Verde vibrante
+    background: '#F9FAFB', // Fundo claro
+    surface: '#FFFFFF', // Branco para cartões e componentes elevados
+    textPrimary: '#1F2937', // Texto escuro principal
+    textSecondary: '#4B5563', // Texto escuro secundário
+  },
+
+  dark: {
+    primary: '#10B981', // Mesma cor primária
+    background: '#1F2937', // Fundo escuro
+    surface: '#374151', // Cinza escuro para cartões e componentes elevados
+    textPrimary: '#D1FAE5', // Texto claro principal
+    textSecondary: '#A7F3D0', // Texto secundário claro
+  },
+
+  finance: {
+    income: '#34D399', // Cor para receitas (verde claro)
+    expense: '#F87171', // Cor para despesas (vermelho claro)
+    investment: '#60A5FA', // Cor para investimentos (azul claro)
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+import { theme } from './src/themes/index';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  darkMode: 'class', // Ativa o dark mode usando a classe `dark`
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: theme.light,
+        dark: theme.dark,
+        finance: theme.finance,
+      },
+    },
   },
   plugins: [],
 };
